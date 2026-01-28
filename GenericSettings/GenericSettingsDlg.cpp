1563d72696c13c17715ff7aa3706c81307491a1b
xymh <ahxymha@outlook.com>
2026-01-28 13:55:52 +0800

    Up

---
+++ GenericSettings/GenericSettingsDlg.cpp
@@ -0,0 +1,158 @@
+﻿
+// GenericSettingsDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "GenericSettings.h"
+#include "GenericSettingsDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CGenericSettingsDlg 对话框
+
+
+
+CGenericSettingsDlg::CGenericSettingsDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_GENERICSETTINGS_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CGenericSettingsDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_TAB2, m_tab);
+}
+
+BEGIN_MESSAGE_MAP(CGenericSettingsDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_COMMAND(ID_32793, &CGenericSettingsDlg::ExitClicked)
+	ON_COMMAND(ID_32771, &CGenericSettingsDlg::OnAbout)
+END_MESSAGE_MAP()
+
+
+// CGenericSettingsDlg 消息处理程序
+
+BOOL CGenericSettingsDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	m_menu.LoadMenuW(IDR_MENU1);
+	SetMenu(&m_menu);
+
+	m_tab.InsertItem(0, L"About");
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	ShowWindow(SW_NORMAL);
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CGenericSettingsDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CGenericSettingsDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CGenericSettingsDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+void CGenericSettingsDlg::ExitClicked()
+{
+	// TODO: 在此添加命令处理程序代码
+	AfxPostQuitMessage(0);
+}
+
+void CGenericSettingsDlg::OnAbout()
+{
+	// TODO: 在此添加命令处理程序代码
+	CAboutDlg about;
+	about.DoModal();
+}
---
+++ GenericSettings/resource.h
@@ -0,0 +1,47 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 GenericSettings.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_GENERICSETTINGS_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define IDC_TAB2                        1001
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_NEKO32779                    32779
+#define ID_NEKO32780                    32780
+#define ID_NEKO32781                    32781
+#define ID_32782                        32782
+#define ID_NEKO32783                    32783
+#define ID_NEKO32784                    32784
+#define ID_32785                        32785
+#define ID_NEKO32786                    32786
+#define ID_NEKO32787                    32787
+#define ID_NEKO32788                    32788
+#define ID_NEKO32789                    32789
+#define ID_32790                        32790
+#define ID_32791                        32791
+#define ID_32792                        32792
+#define ID_32793                        32793
+#define ID_PLUGIN                       32794
+#define ID_PROVIDE                      32795
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32796
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
